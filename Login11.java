10d6301d02c499767a4472f24b0ef0d905e097dd
cesarguijarro-aurea <42980398+cesarguijarro-aurea@users.noreply.github.com>
2018-09-12 16:08:39 -0500

    Update Login11.java

--- Login11.java
+++ Login11.java
@@ -20,4 +20,55 @@ public class Login11 extends HttpServlet
 		out.println("</html>");
 	        out.close();
   	 }
+	 	 public void doGet(HttpServletRequest re,HttpServletResponse rs)
+			 throws ServletException,IOException
+	 {
+	 	rs.setContentType("text/html");
+		PrintWriter out=rs.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Cyber credit card system</title>");
+		out.println("</head>");
+		out.println("<frameset cols=20%,* border=0 scroll=no>");
+		out.println("     <frame src='http://localhost:8080/servlet/about1' scroll='no'>");
+		out.println("     <frame src='http://localhost:8080/servlet/selectoption' scroll='no' name='pick'>");
+		out.println("</frameset>");
+		out.println("</noframes>");
+		out.println("</html>");
+	        out.close();
+  	 }
+	 	 public void doGet(HttpServletRequest re,HttpServletResponse rs)
+			 throws ServletException,IOException
+	 {
+	 	rs.setContentType("text/html");
+		PrintWriter out=rs.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Cyber credit card system</title>");
+		out.println("</head>");
+		out.println("<frameset cols=20%,* border=0 scroll=no>");
+		out.println("     <frame src='http://localhost:8080/servlet/about1' scroll='no'>");
+		out.println("     <frame src='http://localhost:8080/servlet/selectoption' scroll='no' name='pick'>");
+		out.println("</frameset>");
+		out.println("</noframes>");
+		out.println("</html>");
+	        out.close();
+  	 }
+	 	 public void doGet(HttpServletRequest re,HttpServletResponse rs)
+			 throws ServletException,IOException
+	 {
+	 	rs.setContentType("text/html");
+		PrintWriter out=rs.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Cyber credit card system</title>");
+		out.println("</head>");
+		out.println("<frameset cols=20%,* border=0 scroll=no>");
+		out.println("     <frame src='http://localhost:8080/servlet/about1' scroll='no'>");
+		out.println("     <frame src='http://localhost:8080/servlet/selectoption' scroll='no' name='pick'>");
+		out.println("</frameset>");
+		out.println("</noframes>");
+		out.println("</html>");
+	        out.close();
+  	 }
 }
